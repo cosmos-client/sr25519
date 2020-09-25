c08eb60f093f7e7670e1ee8f96bed8e19a24d5ce
Kimura Yu <33382781+KimuraYu45z@users.noreply.github.com>
2020-09-25 17:27:44 +0900

    update

--- bundle.js
+++ bundle.js
@@ -3,15 +3,16 @@ const fs = require("fs");
 const bytes = fs.readFileSync("pkg/sr25519_bg.wasm");
 fs.writeFileSync(
   "pkg/sr25519_bg.js",
-  `export const base64 = '${bytes.toString("base64")}';`
+  `export const base64 = ${bytes.toString("base64")};`
 );
 
 const src = fs.readFileSync("pkg/sr25519.js").toString("utf-8");
 fs.writeFileSync(
   "pkg/sr25519.js",
-  src.replace(
-    `const path = require('path').join(__dirname, 'sr25519_bg.wasm');
+  `const sr25519_bg = require('./sr25519_bg.js');\n\n` +
+    src.replace(
+      `const path = require('path').join(__dirname, 'sr25519_bg.wasm');
 const bytes = require('fs').readFileSync(path);`,
-    `const bytes = Buffer.from(require('sr25519_bg.js').base64, 'base64');`
-  )
+      "const bytes = Buffer.from(sr25519_bg.base64, 'base64');"
+    )
 );
